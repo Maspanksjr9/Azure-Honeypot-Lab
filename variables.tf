616bda0cb1d82da851203b12e6b6a39a894b78c1
Maspanksjr9 <michaelspanks15@gmail.com>
2024-06-06 16:43:12 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  default     = " East US"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "honeypotlab"
+  description = "Name of the resource groups"
+}
