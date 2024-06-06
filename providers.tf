1d652205f87d17ac9c84f0c38f0a0e3fb1c607b3
Maspanksjr9 <michaelspanks15@gmail.com>
2024-06-06 16:45:17 -0400

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "*****************"
+  tenant_id = "*****************"
+  }
